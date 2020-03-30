1cfe3631a02b9975a59ec85c6c88a37071db057f
Rodrigo Coelho <rodrigogc4@al.insper.edu.br>
2020-03-30 16:27:34 -0300

    Teste 2

--- Craps Insper (EP1).py
+++ Craps Insper (EP1).py
@@ -1,1 +1,38 @@
-print("Hello, world!")
+import random as rd
+
+#Configuraçøes básicas
+fichas_jogador = 1000
+nome = input("Olá jogador, qual o seu nome? ")
+jogar = input("Vamos jogar Craps? (s/n): ")
+
+#Loop para setar jogar True ou False. Vai fazer mais sentido mais para frente quando quisermos fechar o loop grande do jogo
+    while type(jogar) == str:
+        if jogar == 's':
+            jogar = True
+        elif jogar == 'n'
+            jogar = False
+        else:
+            jogar = input("Responda apenas com 's' ou 'n'. Tente novamente: ")
+
+
+
+while jogar:
+
+
+
+
+
+
+def pergunta_aposta():
+    resposta = input('Quantas fichas quer apostar? ')
+
+    while verif = False:
+        verif = verif_aposta()
+
+
+
+def verif_aposta(valor_aposta, montante)
+    if valor_aposta > montante:
+        return False
+    else:
+        return True
